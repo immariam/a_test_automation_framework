e5214a096fc85f87cb56634ca51a3f540046747b
Mariam Sargsyan <mariam@Mariams-MacBook-Pro.local>
2023-11-21 18:22:50 +0000

    add some logging

--- test/specs/offerSearch.e2e.js
+++ test/specs/offerSearch.e2e.js
@@ -9,11 +9,17 @@ describe('Restaurant offers search', () => {
         await expect(await restaurantVouchersPage.noSearchResultsHeading.waitForDisplayed()).toBe(true);
     });
 
-// deliberately failing test cases
+// deliberately failing test cases with additional logging
     it('should not show "no results banner"', async () => {
         await restaurantVouchersPage.open();
+        console.log('Opened restaurant vouchers page');
+
         await restaurantVouchersPage.searchRestaurantOffer('London');
+        console.log('Performed search for London');
+
         await expect(await restaurantVouchersPage.expiredInfoBanner.waitForDisplayed()).toBe(false);
+        console.log('Verified that the "expiredInfoBanner" is not displayed');
+
         // Introduce a pause with a promise to allow time for the screenshot to be taken
         await new Promise(resolve => setTimeout(resolve, 5000));
     });
